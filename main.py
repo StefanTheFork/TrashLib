99ab4b6d5643ce19dee769847345818312883009
Stefan The Fork <124001257+StefanTheFork@users.noreply.github.com>
2023-02-19 01:38:26 +0200

    Add files via upload
    added language file and example thing showing most functions

---
+++ main.py
@@ -0,0 +1,20 @@
+
+
+def start():
+    print("script started")
+
+def terminalout(msg):
+    print(msg)
+
+def mathplus(int1,int2):
+    print(int1 + int2 )
+
+def var(value):
+    print(value)
+
+
+def mathminus(int1,int2):
+    print(int1 - int2 )
+
+def end():
+    print("script ended")
---
+++ yes.py
@@ -0,0 +1,13 @@
+import main
+
+main.start()
+
+main.terminalout(msg="worst programming language")
+
+main.mathplus(int1=12,int2=12)
+
+main.mathminus(int1=12,int2=12)
+
+main.var(value="wow")
+
+main.end()
